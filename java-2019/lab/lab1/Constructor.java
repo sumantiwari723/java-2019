cc692950e8cb8b05addf090036a4068ae01f6ceb
Prashantbastola <81133692+Prashantbastola@users.noreply.github.com>
2021-12-09 08:41:16 +0545

    Add files via upload

---
+++ java-2019/lab/lab1/Constructor.java
@@ -0,0 +1,8 @@
+class Constructor{
+	public Constructor(){
+		System.out.println("default constructor");
+	}
+	public static void main(String[]args){
+	    Constructor defaultConstructor = new Constructor();
+	}
+}
