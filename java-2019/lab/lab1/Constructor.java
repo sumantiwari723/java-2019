cbd9fce3931a285a92d4e3367cf7f133a44f3155
Prashantbastola <81133692+Prashantbastola@users.noreply.github.com>
2021-12-09 08:42:28 +0545

    Delete java-2019/lab/lab1 directory

--- java-2019/lab/lab1/Constructor.java
+++
@@ -1,8 +0,0 @@
-class Constructor{
-	public Constructor(){
-		System.out.println("default constructor");
-	}
-	public static void main(String[]args){
-	    Constructor defaultConstructor = new Constructor();
-	}
-}
